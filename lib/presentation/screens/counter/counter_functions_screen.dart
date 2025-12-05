4bcbd530b8954e3750d43148210ca436f0dc2cea
fernandoiosono <fernandoiosono@gmail.com>
2025-12-05 11:56:49 -0600

    CustomFloatingActionButton Widget Created

--- lib/presentation/screens/counter/counter_functions_screen.dart
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -14,64 +14,83 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('Counter Functions', style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.white
-        ),)),
+        title: const Center(
+          child: Text(
+            'Counter Functions',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
         backgroundColor: Colors.blue,
         actions: [
           IconButton(
-            icon: Icon(Icons.refresh_rounded), 
+            icon: Icon(Icons.refresh_rounded),
             onPressed: () {
               setState(() {
                 clickCounter = 0;
               });
-            }
+            },
           ),
         ],
         // leading: IconButton( // Para ubicar los iconos del lado izquierdo del title
-        //   icon: Icon(Icons.refresh_rounded), 
+        //   icon: Icon(Icons.refresh_rounded),
         //   onPressed: () {}
         // ),
       ),
-      body: Center( // Centrado Horizontal
+      body: Center(
+        // Centrado Horizontal
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, // Centrado Vertical
           children: [
-            Text('$clickCounter', style: const TextStyle(
-              fontSize: 160,
-              fontWeight: FontWeight.w100
-            ),),
-            Text('Click${ clickCounter == 1 ? '' : 's'}', style: TextStyle(
-              fontSize: 25
-            ),)
+            Text(
+              '$clickCounter',
+              style: const TextStyle(
+                fontSize: 160,
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+            Text(
+              'Click${clickCounter == 1 ? '' : 's'}',
+              style: TextStyle(fontSize: 25),
+            ),
           ],
-        )
+        ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          FloatingActionButton(
-            shape: const StadiumBorder(),
-            onPressed: () {
-              clickCounter ++;
-              
-              setState((){});
-            },
-            child: const Icon(Icons.plus_one)
-          ),
-          const SizedBox(height: 10),
-          FloatingActionButton(
-            shape: const StadiumBorder(),
-            onPressed: () {
-              if (clickCounter > 0) clickCounter --;
-              
-              setState((){});
-            },
-            child: const Icon(Icons.exposure_minus_1_outlined)
-          )
+          CustomFloatingActionButton(icon: Icons.plus_one),
+          const SizedBox(height: 10), // Separación entre los Botones
+          CustomFloatingActionButton(icon: Icons.exposure_minus_1_outlined),
         ],
-      )
+      ),
+    );
+  }
+}
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final IconData icon;
+
+  const CustomFloatingActionButton({
+    super.key, 
+    required this.icon
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      shape: const StadiumBorder(),
+      onPressed: () {
+        // clickCounter++;
+
+        // setState(() {});
+
+        // if (clickCounter > 0) {
+        //   clickCounter--;
+
+        //   setState(() {});
+        // }
+      },
+      child: Icon(icon),
     );
   }
 }

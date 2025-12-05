c205de61d8a4c367584fa0314b050368d30a4fe1
fernandoiosono <fernandoiosono@gmail.com>
2025-12-05 09:44:32 -0600

    Plus One Button Basic Functionality Finished

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: const Text('Counter Screen', style: TextStyle(
+        title: const Center(child: Text('Counter Screen', style: TextStyle(
           fontWeight: FontWeight.bold,
           color: Colors.white
         ),)),
@@ -16,12 +23,12 @@ class CounterScreen extends StatelessWidget {
       body: Center( // Centrado Horizontal
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, // Centrado Vertical
-          children: const [
-            Text('0', style: TextStyle(
+          children: [
+            Text('$clickCounter', style: const TextStyle(
               fontSize: 160,
               fontWeight: FontWeight.w100
             ),),
-            Text('Clicks', style: TextStyle(
+            const Text('Clicks', style: TextStyle(
               fontSize: 25
             ),)
           ],
@@ -29,7 +36,8 @@ class CounterScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
+          clickCounter ++;
+          setState((){});
         },
         child: const Icon(Icons.plus_one)
       )
